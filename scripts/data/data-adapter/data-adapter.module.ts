e440e76c7769e9ca3a0895cd8874149a23b21e97
Eduard Januzi <eduard.j.89@gmail.com>
2024-07-29 15:39:30 +0200

    Update Csv-s

--- scripts/data/data-adapter/data-adapter.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataAdapterService } from './data-adapter.service';
-import { ProjectsRepository } from '~modules/projects/projects.repository';
-
-@Module({
-  providers: [DataAdapterService, ProjectsRepository],
-})
-export class DataAdapterModule {}
